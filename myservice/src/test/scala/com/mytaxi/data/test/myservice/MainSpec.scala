ac172e8ef47d5e239dfc995979d12a34f0ef4af4
Fatima Shahid. fatima.shahid <fatima.shahid@northbaysolutions.net>
2019-12-05 22:23:44 +0500

    Stream Processing

--- myservice/src/test/scala/com/mytaxi/data/test/myservice/MainSpec.scala
+++
@@ -1,9 +0,0 @@
-package com.mytaxi.data.test.myservice
-
-import org.scalatest._
-
-class MainSpec extends FlatSpec with Matchers {
-  "There is at least one test for this project" should "be true" in {
-    0 should equal(0)
-  }
-}
